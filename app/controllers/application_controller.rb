ec1ebff8d15afaa54bc4928416bd51ea44d71233
Ebuka Umeokonkwo <ebukaume@gmail.com>
2019-11-18 17:35:40 +0100

    add cors preflight policy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
-  before_filter :cors_preflight_check
-  after_filter :cors_set_access_control_headers
+  before_action :cors_preflight_check
+  after_action :cors_set_access_control_headers
 
   # For all responses in this controller, return the CORS access control headers.
 
