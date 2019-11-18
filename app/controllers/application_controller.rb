590291d5a42e5e70544c3a207922aacf2f97534f
Ebuka Umeokonkwo <ebukaume@gmail.com>
2019-11-18 16:29:26 +0100

    allow cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  before_action :white_list_cors
+
+  def white_list_cors
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, DELETE, POST'
+    response.headers['Access-Control-Allow-Headers'] = 'origin, content-type, accept'
+  end
 end
