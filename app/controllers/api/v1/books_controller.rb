f7d473e855e62e492b0b53296a7e2a1075ac9f66
Ebuka Umeokonkwo <ebukaume@gmail.com>
2019-11-19 09:27:48 +0100

    modify response to react book after POST and DELETE

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,34 +1,34 @@
 class Api::V1::BooksController < ApplicationController
   def index
-    render json: get_all_books('success')
+    render json: get_all_books('SUCCESS')
   end
 
   def create
-    @book = Book.new(book_params)
-    if @book.save
-      render json: get_all_books('success')
+    book = Book.new(book_params)
+    if book.save
+      render json: {status:'SUCCESS', book: book}
     else
-      render json: get_all_books('failure')
+      render json: {status:'FAILURE', book: book}
     end
   end
 
   def destroy
-    @book = Book.find(params[:id])
-    if @book.destroy
-      render json: get_all_books('success')
+    book = Book.find(params[:id])
+    if book.destroy
+      render json: {status:'SUCCESS', book: book}
     else
-      render json: get_all_books('failure')
+      render json: {status:'FAILURE', book: book}
     end
-
   end
+
   private
 
-  def get_all_books(status)
-    books = Book.all.as_json(only: [:id, :title, :author, :category])
-    {status:status, books: books}
-  end
+    def get_all_books(status)
+      books = Book.all.as_json(only: [:id, :title, :author, :category])
+      {status:status, books: books}
+    end
 
-  def book_params
-    params.require(:book).permit(:title, :author, :category)
-  end
+    def book_params
+      params.require(:book).permit(:title, :author, :category)
+    end
 end
